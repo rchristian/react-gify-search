fe007a455b6e637f10ee6f364257f55f2d86cb8d
Rachel <racheldchristian@gmail.com>
2018-03-10 20:14:40 -0600

    Add copy to clipboard functionality

--- src/components/GifPopup.js
+++ src/components/GifPopup.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const GifPopup = (props) => {
-	return (
-		<div className="popup-wrap">
-			<div className="popup-content">
-				<span className="close-btn" onClick={props.closePopup}>x</span>
-				<img src={props.selectedGif.images.original.url} alt='' />
-				<input type="text" value={props.selectedGif.source} readOnly />
-				<button className="copy">Copy</button>
+class GifPopup extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			copied: false,
+		}
+	}
+
+	onCopy = () => {
+		this.setState({copied: true});
+	}
+
+	render(props) {
+		return (
+			<div className="popup-wrap">
+				<div className="popup-content">
+					<span className="close-btn" onClick={this.props.closePopup}>x</span>
+					<img src={this.props.selectedGif.images.original.url} alt='' />
+					<input type="text" value={this.props.selectedGif.source} readOnly />
+					<CopyToClipboard text={this.props.selectedGif.source} onCopy={this.onCopy}>
+						<button className="copy-btn">Copy</button>
+					</CopyToClipboard>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default GifPopup;
