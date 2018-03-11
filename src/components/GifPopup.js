42a68b9190fee207802d03996924ca1cbee27821
Rachel <racheldchristian@gmail.com>
2018-03-10 21:36:47 -0600

    Add popup button stylings

--- src/components/GifPopup.js
+++ src/components/GifPopup.js
@@ -20,9 +20,20 @@ class GifPopup extends React.Component {
 				<div className="popup-content">
 					<span className="close-btn" onClick={this.props.closePopup}>x</span>
 					<img src={this.props.selectedGif.images.original.url} alt='' />
-					<input type="text" value={this.props.selectedGif.source} readOnly />
+					<input className="gif-link" type="text" value={this.props.selectedGif.source} readOnly />
 					<CopyToClipboard text={this.props.selectedGif.source} onCopy={this.onCopy}>
-						<button className="copy-btn">Copy</button>
+						<button className="copy-btn">
+							<svg xmlns="http://www.w3.org/2000/svg" 
+								 viewBox="0 0 24 24" 
+								 fill="none" 
+								 stroke="currentColor" 
+								 strokeWidth="2" 
+								 strokeLinecap="round" 
+								 strokeLinejoin="round" >
+								 	<path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+								 	<rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+							</svg>
+						</button>
 					</CopyToClipboard>
 				</div>
 			</div>
