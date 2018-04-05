1b0530aaa506652a19061b54e8e93a209a10fde1
Rachel <racheldchristian@gmail.com>
2018-04-05 17:54:52 -0500

    Add Grid, Rows, and Cols from react bootstrap

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class SearchBar extends React.Component {
 	constructor() {
@@ -13,11 +15,21 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div className="search">
-				<input onChange={e => this.onInputChange(e.target.value)} />
-			</div>
+			<Grid>
+				<Row>
+					<Col xs={12} md={12} ls={12}>
+						<div className="search">
+							<input onChange={e => this.onInputChange(e.target.value)} />
+						</div>
+					</Col>
+				</Row>
+			</Grid>
 		);
 	}
+}
+
+SearchBar.PropTypes = {
+	onTermChange: PropTypes.func.isRequired,
 }
 
 export default SearchBar;
